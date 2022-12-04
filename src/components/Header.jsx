1a95725b2a06480cd140d815ae823d3a877bc9f4
jihye <jiji.pak41@gmail.com>
2022-12-04 22:22:31 +0000

    ✨ Created header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { ShoppingBagOpen, ShoppingCartSimple, Pencil } from 'phosphor-react';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
   return (
     <header>
-      <div>Shop</div>
+      <div>
+        <ShoppingBagOpen />
+        <p>Shop</p>
+      </div>
+      <div>
+        <span onClick={() => navigate('/products')}>Products</span>
+        <ShoppingCartSimple
+          onClick={() => {
+            navigate('/my-cart');
+          }}
+        />
+        <Pencil />
+        <span>User Img</span>
+        <span>UserName</span>
+        <button>Login/Logout</button>
+      </div>
     </header>
   );
 }
