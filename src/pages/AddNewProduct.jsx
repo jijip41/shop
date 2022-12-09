e35d7e32ec7391e3c214d01b963dd74295b86a25
jihye <jiji.pak41@gmail.com>
2022-12-09 16:57:53 +0000

    ✨ Create a form

--- src/pages/AddNewProduct.jsx
+++ src/pages/AddNewProduct.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
 function AddNewProduct() {
-  return <div> Add new products</div>;
+  return (
+    <main className="flex flex-col">
+      <h2> Add new products</h2>
+      <form action="" method="get" className="flex flex-col w-md">
+        <input type="file" alt=""></input>
+        <input
+          type="text"
+          className="border-2 border-black"
+          placeholder="Name"
+        ></input>
+        <input
+          type="text"
+          className="border-2 border-black"
+          placeholder="Price"
+        ></input>
+        <input
+          type="text"
+          className="border-2 border-black"
+          placeholder="Category"
+        ></input>
+        <input
+          type="text"
+          className="border-2 border-black"
+          placeholder="Desctiption"
+        ></input>
+        <input
+          type="text"
+          className="border-2 border-black"
+          placeholder="Options"
+        ></input>
+        <button>Add product</button>
+      </form>
+    </main>
+  );
 }
 
 export default AddNewProduct;
