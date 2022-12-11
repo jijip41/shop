2eb2fdc485c2d1cb1637b46f7e438b3afb5b6294
jihye <jiji.pak41@gmail.com>
2022-12-11 01:30:57 +0000

    ✨ Update cloudinary api

---
+++ src/api/uploader.js
@@ -0,0 +1,12 @@
+export async function uploadImg(file) {
+  const data = new FormData();
+  data.append('file', file);
+  data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET);
+
+  return fetch(process.env.REACT_APP_CLOUDINARY_UPLOAD_URL, {
+    method: 'POST',
+    body: data,
+  })
+    .then((res) => res.json())
+    .then((data) => data.url);
+}
