1bca2a95e63a02dd7f740bb1e9fa91e9834f79ea
jihye <jiji.pak41@gmail.com>
2022-12-12 01:59:41 +0000

    ✨ Add ProductDetail component

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import Button from '../components/Button';
 
 function ProductDetail() {
-  return <div>product details</div>;
+  const {
+    state: {
+      product: { id, imageUrl, name, description, category, options, price },
+    },
+  } = useLocation();
+  const [optionSelected, setOptionSelected] = useState(options[0]);
+
+  const handleSubmit = (e) => setOptionSelected(e.target.value);
+  return (
+    <>
+      <p>Category {`> ${category}`}</p>
+      <section className="flex flex-col md:flex-row p-4 gap-2">
+        <div className="content-center w-full basis-7/12">
+          <img src={imageUrl} alt={name} className="w-full"></img>
+        </div>
+        <div className="w-full basis-5/12 flex flex-col pl-4">
+          <p className="text-2xl font-bold text-zinc-800">{name}</p>
+          <p className="text-xl font-bold text-zinc-600">£ {price}</p>
+          <p className="text-zinc-500">{description}</p>
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col w-40 space-y-2"
+          >
+            <label htmlFor="options" className="text-rose-500">
+              Select an option:
+            </label>
+            <select
+              name="options"
+              id="options"
+              className="outline-none border-2 border-dashed border-rose-400"
+            >
+              {options.map((option) => (
+                <option value={optionSelected} key={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <Button content="Add to Cart" />
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default ProductDetail;
