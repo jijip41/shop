9e8e274076749f9153c1901bb88a0d5624b05b84
jihye <jiji.pak41@gmail.com>
2022-12-12 01:58:46 +0000

    ♻️ Pass state to a product detail page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function ProductCard({ product: { id, name, imageUrl, price } }) {
+function ProductCard({
+  product,
+  product: { id, name, imageUrl, price, category },
+}) {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`products/${id}`, { state: { product } });
+  };
   return (
-    <li className="border rounded-md border-rose-200 flex flex-col items-center space-y-2 p-4 cursor-pointer">
+    <li
+      className="border rounded-md border-rose-200 flex flex-col items-center space-y-2 p-4 cursor-pointer"
+      onClick={handleClick}
+    >
       <img src={imageUrl} className="w-full h-auto" alt=""></img>
       <p>{name}</p>
       <p>£{price}</p>
