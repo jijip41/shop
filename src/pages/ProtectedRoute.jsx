45b6add3f8cd4ad9f288ab85add15df7e3f8e3e3
jihye <jiji.pak41@gmail.com>
2022-12-19 13:32:05 +0000

    🚚 Move context forder out of components folder

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuthContext } from '../components/context/AuthContext';
+import { useAuthContext } from '../context/AuthContext';
 
 function ProtectedRoute({ children, requiredAdmin }) {
   const { user } = useAuthContext();
